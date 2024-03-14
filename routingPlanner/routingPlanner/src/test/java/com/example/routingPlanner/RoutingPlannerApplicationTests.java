04d0bbca4bfe79efa9807cf5c1a1d0e2f58d36d3
Fuchs Julian <julian.fuchs0701@gmail.com>
2024-03-14 14:11:13 +0100

    Initial Commit
    Springboot Initializr Setup

---
+++ routingPlanner/routingPlanner/src/test/java/com/example/routingPlanner/RoutingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.routingPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingPlannerApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
