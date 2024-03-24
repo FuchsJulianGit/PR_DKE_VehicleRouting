3a0706f79efdcae987c8d34137a8134822321fd8
Fuchs Julian <julian.fuchs0701@gmail.com>
2024-03-24 16:30:06 +0100

    Map with working Coordinates

--- routingPlanner_frontend/src/app/map/map.component.ts
+++ routingPlanner_frontend/src/app/map/map.component.ts
@@ -10,17 +10,18 @@ import 'leaflet-routing-machine';
 })
 
 export class MapComponent implements AfterViewInit {
-  private map!: L.Map;
- 
- 
 
-  markers: L.Marker[] = [
-    L.marker([31.9539, 35.9106]),
-    L.marker([32.5568, 35.8469]),
-
-    L.marker([31.9839, 35.7106]),
-    L.marker([32.7568, 35.4469]) 
+  public routesData = [
+    { routeName: "Route 1", sequenceNumber: 1, coordinates: ["-22.985308", "-43.204845"] },
+    { routeName: "Route 1", sequenceNumber: 2, coordinates: ["-22.983449", "-43.202773"] },
+    { routeName: "Route 2", sequenceNumber: 1, coordinates: ["-22.980308", "-43.214845"] },
+    { routeName: "Route 2", sequenceNumber: 2, coordinates: ["-22.973449", "-43.212773"] },
+    { routeName: "Route 2", sequenceNumber: 3, coordinates: ["-22.993449", "-43.192773"] }
   ];
+  
+
+  private map!: L.Map;
+  private markers: L.Marker[] = [];
 
   constructor() { }
 
@@ -30,101 +31,90 @@ export class MapComponent implements AfterViewInit {
   ngAfterViewInit() {
     this.initializeMap();
     this.addMarkers();
-    this.centerMap();
     this.initializeRouting();
   }
 
-
   private initializeMap() {
-    //https://gist.github.com/xantiagoma/39145a3042eca53a57ac3290a1a34973?permalink_comment_id=3415377
-    //const baseMapURl = 'http://mts3.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}';
-    //const baseMapURl = "https://mts1.google.com/vt/lyrs=m@186112443&hl=x-local&src=app&x=1325&y=3143&z=13&s=Galile";
-    //const baseMapURl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-    //const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?key=2IbwsVHJNBrcYUp0xEJs'
     const baseMapURl = 'https://tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=9EPuY0KcdIk7nGeNgjz84t4v6YXWvE33qACJMNKYfX0m1UfytsyWERuzVJ3rR7Sk'
     this.map = L.map('map').setView([31.9539, 35.9106],  1);
     L.tileLayer(baseMapURl).addTo(this.map);
-  
   }
-
 
   private addMarkers() {
-    // Add your markers to the map
-    this.markers.forEach(marker => marker.addTo(this.map));
+    this.markers.forEach(marker => marker.removeFrom(this.map));
+    this.markers = [];
+    this.routesData.forEach(routeData => {
+      var coordinates: L.LatLngTuple = [Number(routeData.coordinates[0]), Number(routeData.coordinates[1])];
+    });
   }
 
-  private centerMap() {
-    // Create a LatLngBounds object to encompass all the marker locations
-    const bounds = L.latLngBounds(this.markers.map(marker => marker.getLatLng()));
-    
-    // Fit the map view to the bounds
-    this.map.fitBounds(bounds);
+  public getPoints(routeData: any): number[][] {
+    console.dir(routeData);
+    console.log("Route 0: " + routeData[0]);
+    console.log("Route 1: " + routeData[1]);
+
+    var coordinates: number[][] = [];
+
+    routeData.forEach((data: any) => {
+      const lat = Number(data.coordinates[0]);
+      const lng = Number(data.coordinates[1]);
+      coordinates.push([lat, lng]);
+    });
+
+    console.dir(coordinates);
+
+    return coordinates;
   }
 
   private initializeRouting() {
-    const waypoints = [
-      L.latLng(-22.985308, -43.204845),
-      L.latLng(-22.983449, -43.202773),
-    ];
+    const groupedRoutes = this.groupRoutesByRouteName(this.routesData);
 
-    const waypoints1 = [
-      L.latLng(-22.983449, -43.204845),
-      L.latLng(-22.984708, -43.202430),
-    ];
+    Object.values(groupedRoutes).forEach((routes: any[], index) => {
 
-    var route1 = L.Routing.control({
-      waypoints: [
-          L.latLng(-22.985308, -43.204845),
-          L.latLng(-22.983449, -43.202773),
-      ],
-      lineOptions: {
-        styles: [{ color: '#5733ff', opacity: 1, weight: 3 }],
-        extendToWaypoints: true,
-        missingRouteTolerance: 10
-    }
-  }).addTo(this.map);
-  
-  var route2 = L.Routing.control({
-      waypoints: [
-          L.latLng(-22.983449, -43.204845),
-          L.latLng(-22.984708, -43.202430),
-      ],
-      lineOptions: {
-        styles: [{ color: '#ff5733', opacity: 1, weight: 3 }],
-        extendToWaypoints: true,
-        missingRouteTolerance: 10
+      var group = L.featureGroup();
+      var latlngArray = [];
+      var input = this.getPoints(routes);
+      var innerRoute = [];
+      
+      console.log("input: " + input[0]);
+      console.log("input: " + input[1]);
+
+      for (var i = 0; i < input.length; ++i) {
+        var ltln = L.latLng(input[i][0], input[i][1]);
+        L.circleMarker(ltln, {
+          radius: 2
+        }).addTo(group);
+        latlngArray.push(ltln);
       }
-  }).addTo(this.map);
 
+         innerRoute[index] = L.Routing.control({
+          waypoints: [...latlngArray],
+          lineOptions: {
+            styles: [{ color: index === 0 ? '#5733ff' : '#ff5733', opacity: 1, weight: 3 }],
+            extendToWaypoints: true,
+            missingRouteTolerance: 10
+          }
+        }).addTo(this.map);
 
-/// Remove Tool Tips!
-
-   var routingControlContainer = route1?.getContainer();
-   var controlContainerParent = routingControlContainer?.parentNode;
-  if (controlContainerParent && routingControlContainer) {
-    controlContainerParent.removeChild(routingControlContainer);
-}
- routingControlContainer = route2?.getContainer();
- controlContainerParent = routingControlContainer?.parentNode;
-if (controlContainerParent && routingControlContainer) {
-  controlContainerParent.removeChild(routingControlContainer);
-}
-  
-
-
-
-    /*var routingControl = L.Routing.control({
-      router: L.Routing.osrmv1({
-        serviceUrl: 'http://router.project-osrm.org/route/v1/'
-      }),
-      showAlternatives: true,
-      fitSelectedRoutes: false,
-      show: false,
-      routeWhileDragging: true,
-      waypoints: waypoints
-    }    
-    ).addTo(this.map);*/
-
-    
+      const routingControlContainer = innerRoute[index].getContainer();
+      const controlContainerParent = routingControlContainer?.parentNode;
+      if (controlContainerParent && routingControlContainer) {
+        controlContainerParent.removeChild(routingControlContainer);
+      }
+    });
   }
-}
+
+  private groupRoutesByRouteName(routesData: any[]): { [key: string]: any[] } {
+    const groupedRoutes: { [key: string]: any[] } = {};
+    routesData.forEach(routeData => {
+      const routeName = routeData.routeName;
+      if (!groupedRoutes[routeName]) {
+        groupedRoutes[routeName] = [];
+      }
+      groupedRoutes[routeName].push(routeData);
+    });
+    return groupedRoutes;
+  }
+
+  }
+ 
